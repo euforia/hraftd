985d83c5ae1adf49d3767afba9deba3b3857674a
euforia <euforia@gmail.com>
2016-06-22 11:11:55 -0700

    Added storage interface

--- store/utils.go
+++ store/utils.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"os"
 	"time"
+
+	"github.com/euforia/go-chord"
 )
 
 const (
@@ -13,8 +15,13 @@ const (
 	raftTimeout         = 10 * time.Second
 )
 
+const (
+	opTypeSet byte = iota
+	opTypeDelete
+)
+
 type command struct {
-	Op    string `json:"op,omitempty"`
+	Op    byte   `json:"op,omitempty"`
 	Key   string `json:"key,omitempty"`
 	Value string `json:"value,omitempty"`
 }
@@ -36,4 +43,28 @@ func readPeersJSON(path string) ([]string, error) {
 	}
 
 	return peers, nil
+}
+
+func defaultChordConfig(bindAddr string, vnodeCount int, stabMin, stabMax time.Duration) *chord.Config {
+	cfg := DefaultConfig(*bindAddr)
+
+	if vnodeCount > 0 {
+		cfg.Chord.NumVnodes = vnodeCount
+	}
+
+	// Stabilization
+	cfg.Chord.StabilizeMin = stabMin
+	cfg.Chord.StabilizeMax = stabMax
+}
+
+func createOrJoinChordRing(chordConfig *chord.Config, bindAddr, joinAddr string, timeout time.Duration) (*chord.Ring, error) {
+	trans, err := chord.InitTCPTransport(bindAddr, timeout)
+	if err != nil {
+		return nil, err
+	}
+
+	if joinAddr != "" {
+		return chord.Join(chordConfig, trans, joinAddr)
+	}
+	return chord.Create(chordConfig, trans)
 }
